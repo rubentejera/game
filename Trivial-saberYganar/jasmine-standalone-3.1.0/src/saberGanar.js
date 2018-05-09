ab73a198ff0114a1890b13e1e738c5272a5dcd28
Celia19C <thatcyclone@gmail.com>
2018-05-09 10:23:20 +0200

    little bit of css

--- Trivial-saberYganar/jasmine-standalone-3.1.0/src/saberGanar.js
+++ Trivial-saberYganar/jasmine-standalone-3.1.0/src/saberGanar.js
@@ -5,54 +5,54 @@ function start() {
         id: 1,
         question: "¿Cuál es la capital de Portugal?",
         answers: [
-            { id: 1, answer: "Faro", isCorrect: false, idQuestion: 1 },
-            { id: 2, answer: "Oporto", isCorrect: false, idQuestion: 1 },
-            { id: 3, answer: "Lisboa", isCorrect: true, idQuestion: 1 }
+            { id: 0, answer: "Faro", isCorrect: false, idQuestion: 1 },
+            { id: 1, answer: "Oporto", isCorrect: false, idQuestion: 1 },
+            { id: 2, answer: "Lisboa", isCorrect: true, idQuestion: 1 }
         ]
     },
     {
         id: 2,
         question: "¿Cuál es la capital de Egipto?",
         answers: [
-            { id: 1, answer: "Faro", isCorrect: false, idQuestion: 2 },
-            { id: 2, answer: "El Cairo", isCorrect: true, idQuestion: 2 },
-            { id: 3, answer: "Lisboa", isCorrect: false, idQuestion: 2 }
+            { id: 0, answer: "Faro", isCorrect: false, idQuestion: 2 },
+            { id: 1, answer: "El Cairo", isCorrect: true, idQuestion: 2 },
+            { id: 2, answer: "Lisboa", isCorrect: false, idQuestion: 2 }
         ]
     },
     {
         id: 3,
         question: "¿Cuál es la capital de España?",
         answers: [
-            { id: 1, answer: "Madrid", isCorrect: true, idQuestion: 3 },
-            { id: 2, answer: "Oporto", isCorrect: false, idQuestion: 3 },
-            { id: 3, answer: "Lisboa", isCorrect: false, idQuestion: 3 }
+            { id: 0, answer: "Madrid", isCorrect: true, idQuestion: 3 },
+            { id: 1, answer: "Oporto", isCorrect: false, idQuestion: 3 },
+            { id: 2, answer: "Lisboa", isCorrect: false, idQuestion: 3 }
         ]
     },
     {
         id: 4,
         question: "¿Cuál es la capital de Zambia?",
         answers: [
-            { id: 1, answer: "Lusaka", isCorrect: true, idQuestion: 4 },
-            { id: 2, answer: "Oporto", isCorrect: false, idQuestion: 4 },
-            { id: 3, answer: "Lisboa", isCorrect: false, idQuestion: 4 }
+            { id: 0, answer: "Lusaka", isCorrect: true, idQuestion: 4 },
+            { id: 1, answer: "Oporto", isCorrect: false, idQuestion: 4 },
+            { id: 2, answer: "Lisboa", isCorrect: false, idQuestion: 4 }
         ]
     },
     {
         id: 5,
         question: "¿Cuál es la capital de Jordania?",
         answers: [
-            { id: 1, answer: "Madrid", isCorrect: false, idQuestion: 5 },
-            { id: 2, answer: "Amán", isCorrect: true, idQuestion: 5 },
-            { id: 3, answer: "Lisboa", isCorrect: false, idQuestion: 5 }
+            { id: 0, answer: "Madrid", isCorrect: false, idQuestion: 5 },
+            { id: 1, answer: "Amán", isCorrect: true, idQuestion: 5 },
+            { id: 2, answer: "Lisboa", isCorrect: false, idQuestion: 5 }
         ]
     },
     {
         id: 6,
         question: "¿Cuál es la capital de Panama?",
         answers: [
-            { id: 1, answer: "Madrid", isCorrect: false, idQuestion: 6 },
-            { id: 2, answer: "Oporto", isCorrect: false, idQuestion: 6 },
-            { id: 3, answer: "Ciudad de Panamá", isCorrect: true, idQuestion: 6 }
+            { id: 0, answer: "Madrid", isCorrect: false, idQuestion: 6 },
+            { id: 1, answer: "Oporto", isCorrect: false, idQuestion: 6 },
+            { id: 2, answer: "Ciudad de Panamá", isCorrect: true, idQuestion: 6 }
         ]
     }];
 
@@ -65,8 +65,8 @@ function start() {
 const boxQuestions = document.querySelector('.questions');
 const btn = document.querySelector('.btn');
 const btnNext = document.querySelector('.btnNext');
-let   userAnswerid;
-let currentIdQuestion;
+let msg = document.querySelector('.message');
+
 
 //Sucesión de preguntas cada 20 segundos o cada vez que das al botón.
 var i = 0;
@@ -82,7 +82,7 @@ function goingQuestions() {
         </div>`;
         }
         i++;
-        
+    msg.innerHTML = '';    
     }
 }
 btnNext.addEventListener('click', goingQuestions);
@@ -92,7 +92,6 @@ setInterval(goingQuestions, 20000)
 
 //Seleccionar respuesta
 function readUserAnswer() { 
-    
     const arrayRadioAnswers = document.querySelectorAll('.answer');
     for (var i = 0; i < arrayRadioAnswers.length; i++) {
         if (arrayRadioAnswers[i].checked) {
@@ -104,20 +103,22 @@ function readUserAnswer() {
         if (question.id == questionBox.id){
             return question
         }
-      });     
+    });     
 
-    if (found.answers[optionChecked.id-1].isCorrect == true){
+    if (found.answers[optionChecked.id].isCorrect == true){
         console.log('BIEN')
+        msg.innerHTML = `<h3> ¡Correcta! </h3>`   
     }
     else {
         console.log('MAL')
+        msg.innerHTML = `<h3> ¡Incorrecta! </h3>`
     }
 
 }
 
 
 
-    btn.addEventListener('click', readUserAnswer);
-    // btn.addEventListener('click', goingQuestions)
+btn.addEventListener('click', readUserAnswer);
+// btn.addEventListener('click', goingQuestions)
 
 } start();
