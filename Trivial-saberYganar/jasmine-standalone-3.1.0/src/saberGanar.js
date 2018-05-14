52c969c11fabdae51fbb095fad05d18e7cb6482b
rubentejera <ruben.tejera@leandmind.es>
2018-05-14 15:24:53 +0100

    rename functions printScoreAndName to updateGeneralScoreboardInterface

--- Trivial-saberYganar/jasmine-standalone-3.1.0/src/saberGanar.js
+++ Trivial-saberYganar/jasmine-standalone-3.1.0/src/saberGanar.js
@@ -1,263 +1,255 @@
 function start() {
-'use strict';
+    'use strict';
 
     const questionsWithAnswers = [{
         id: 1,
         question: "¿Cuál es la capital de Portugal?",
         answers: [
-            { id: 0, answer: "Faro", isCorrect: false, idQuestion: 1 },
-            { id: 1, answer: "Oporto", isCorrect: false, idQuestion: 1 },
-            { id: 2, answer: "Lisboa", isCorrect: true, idQuestion: 1 }
+            {id: 0, answer: "Faro", isCorrect: false, idQuestion: 1},
+            {id: 1, answer: "Oporto", isCorrect: false, idQuestion: 1},
+            {id: 2, answer: "Lisboa", isCorrect: true, idQuestion: 1}
         ]
     },
-    {
-        id: 2,
-        question: "¿Cuál es la capital de Egipto?",
-        answers: [
-            { id: 0, answer: "Faro", isCorrect: false, idQuestion: 2 },
-            { id: 1, answer: "El Cairo", isCorrect: true, idQuestion: 2 },
-            { id: 2, answer: "Lisboa", isCorrect: false, idQuestion: 2 }
-        ]
-    },
-    {
-        id: 3,
-        question: "¿Cuál es la capital de Zambia?",
-        answers: [
-            { id: 0, answer: "Lusaka", isCorrect: true, idQuestion: 4 },
-            { id: 1, answer: "Oporto", isCorrect: false, idQuestion: 4 },
-            { id: 2, answer: "Lisboa", isCorrect: false, idQuestion: 4 }
-        ]
-    },
-    {
-        id: 4,
-        question: "¿Cuál es la capital de Jordania?",
-        answers: [
-            { id: 0, answer: "Madrid", isCorrect: false, idQuestion: 5 },
-            { id: 1, answer: "Amán", isCorrect: true, idQuestion: 5 },
-            { id: 2, answer: "Lisboa", isCorrect: false, idQuestion: 5 }
-        ]
-    },
-    {
-        id: 5,
-        question: "¿Cuál es la capital de Panama?",
-        answers: [
-            { id: 0, answer: "Madrid", isCorrect: false, idQuestion: 6 },
-            { id: 1, answer: "Oporto", isCorrect: false, idQuestion: 6 },
-            { id: 2, answer: "Ciudad de Panamá", isCorrect: true, idQuestion: 6 }
-        ]
-    }];
+        {
+            id: 2,
+            question: "¿Cuál es la capital de Egipto?",
+            answers: [
+                {id: 0, answer: "Faro", isCorrect: false, idQuestion: 2},
+                {id: 1, answer: "El Cairo", isCorrect: true, idQuestion: 2},
+                {id: 2, answer: "Lisboa", isCorrect: false, idQuestion: 2}
+            ]
+        },
+        {
+            id: 3,
+            question: "¿Cuál es la capital de Zambia?",
+            answers: [
+                {id: 0, answer: "Lusaka", isCorrect: true, idQuestion: 4},
+                {id: 1, answer: "Oporto", isCorrect: false, idQuestion: 4},
+                {id: 2, answer: "Lisboa", isCorrect: false, idQuestion: 4}
+            ]
+        },
+        {
+            id: 4,
+            question: "¿Cuál es la capital de Jordania?",
+            answers: [
+                {id: 0, answer: "Madrid", isCorrect: false, idQuestion: 5},
+                {id: 1, answer: "Amán", isCorrect: true, idQuestion: 5},
+                {id: 2, answer: "Lisboa", isCorrect: false, idQuestion: 5}
+            ]
+        },
+        {
+            id: 5,
+            question: "¿Cuál es la capital de Panama?",
+            answers: [
+                {id: 0, answer: "Madrid", isCorrect: false, idQuestion: 6},
+                {id: 1, answer: "Oporto", isCorrect: false, idQuestion: 6},
+                {id: 2, answer: "Ciudad de Panamá", isCorrect: true, idQuestion: 6}
+            ]
+        }];
 
 
-
-
-
-
-
-const boxQuestions = document.querySelector('.questions');
-const btnSend      = document.querySelector('.btn');
+    const boxQuestions = document.querySelector('.questions');
+    const btnSend = document.querySelector('.btn');
 // const btnNext      = document.querySelector('.btnNext');
-const btnStart     = document.querySelector('.btnStart');
-const btnSave      = document.querySelector('.btnSave');
-let msg            = document.querySelector('.message');
-let clock          = document.querySelector('.seconds');
-let nameBox        = document.querySelector('.nameBox');
-let scoreUI        = document.querySelector('.scoreUI');
-let totalPoints    = 0;
-let seconds        = 0;
-let i              = 0;
-let sumPoints;
-let listNames;
-let found;
-let optionChecked;
-let timer;
-btnSend.disabled   = true;
-
+    const btnStart = document.querySelector('.btnStart');
+    const btnSave = document.querySelector('.btnSave');
+    let msg = document.querySelector('.message');
+    let clock = document.querySelector('.seconds');
+    let nameBox = document.querySelector('.nameBox');
+    let scoreUI = document.querySelector('.scoreUI');
+    let totalPoints = 0;
+    let seconds = 0;
+    let i = 0;
+    let sumPoints;
+    let listNames;
+    let found;
+    let optionChecked;
+    let timer;
+    btnSend.disabled = true;
 
 
 //INICIALIZA el juego y el tiempo
 
-btnStart.addEventListener('click',onStart);
+    btnStart.addEventListener('click', onStart);
 
-function onStart() {
-    btnStart.classList.toggle('invisible');
-    btnSend.classList.toggle('invisible');
-    boxQuestions.classList.remove('invisible');
-    i = 0;
-    paintQuestions();
-    timer = setInterval(updateTimer, 1000) //El setInterval en una variable par luego utilizarla con el clearInterval
-}
+    function onStart() {
+        btnStart.classList.toggle('invisible');
+        btnSend.classList.toggle('invisible');
+        boxQuestions.classList.remove('invisible');
+        i = 0;
+        paintQuestions();
+        timer = setInterval(updateTimer, 1000) //El setInterval en una variable par luego utilizarla con el clearInterval
+    }
 
 
 //SUCESIÓN DE PREGUNTAS
 
-function paintQuestions() {
-    if (i < questionsWithAnswers.length) {
-        boxQuestions.innerHTML =
-        `<div class="questionBox" id="${questionsWithAnswers[i].id}">${questionsWithAnswers[i].question}</div>`;
-        for (let x = 0; x < questionsWithAnswers[i].answers.length; x++) {
-            boxQuestions.innerHTML +=
-                `<div class="checkboxBox">
+    function paintQuestions() {
+        if (i < questionsWithAnswers.length) {
+            boxQuestions.innerHTML =
+                `<div class="questionBox" id="${questionsWithAnswers[i].id}">${questionsWithAnswers[i].question}</div>`;
+            for (let x = 0; x < questionsWithAnswers[i].answers.length; x++) {
+                boxQuestions.innerHTML +=
+                    `<div class="checkboxBox">
             <input type="radio" id="${questionsWithAnswers[i].answers[x].id}" name="options" class="answer" value="${questionsWithAnswers[i].answers[x].answer}"/>
         <label for="${questionsWithAnswers[i].answers[x].id}">${questionsWithAnswers[i].answers[x].answer}</label>
         </div>`;
+            }
+            i++;
+            msg.innerHTML = '';
+        } else {
+            nameBox.classList.toggle('invisible');
+            btnSend.disabled = true; //Se desabilita cuando llega al final de las preguntas
+            stopTimer()
         }
-        i++;
-    msg.innerHTML = '';
-    } else {
-        nameBox.classList.toggle('invisible');
-        btnSend.disabled = true; //Se desabilita cuando llega al final de las preguntas 
-        stopTimer()
     }
-}
+
 //Set interval con la función startTimer para que cada segundo compruebe que los segundos no han llegado a 20.
 //Si llega a 20 ejecuta la función de pintar las preguntas, es decir, pasa a la siguiente y resta 3 puntos.
 
-function updateTimer() {
-    seconds++;
-    clock.innerHTML= `${seconds}`;
-    if (seconds === 20) {
-        seconds = 0;
-        paintQuestions();
-        totalPoints -=3;
-        console.log(totalPoints);
-        printScoreUIRealTime();
+    function updateTimer() {
+        seconds++;
+        clock.innerHTML = `${seconds}`;
+        if (seconds === 20) {
+            seconds = 0;
+            paintQuestions();
+            totalPoints -= 3;
+            console.log(totalPoints);
+            updateScoreInterface();
+        }
+        enable();//Comprueba cada segundo si hay algún check seleccionado para habilitar el botón (cada segundo por el setInterval)
     }
-    enable();//Comprueba cada segundo si hay algún check seleccionado para habilitar el botón (cada segundo por el setInterval)
-}
 
 //RESETEA temporizador y puntos y lo deja preparado para iniciar el juego de nuevo
 
-    function stopTimer(){
+    function stopTimer() {
         seconds = 0;
         clearInterval(timer);
     }
 
-    function resetTimeAndPoints(){
+    function resetTimeAndPoints() {
         totalPoints = 0;
-        printScoreUIRealTime();
+        updateScoreInterface();
         stopTimer();
         clock.innerHTML = '';
     }
 
 
-function enable(){
-    btnSend.disabled = true;
-    const arrayRadioAnswers = document.querySelectorAll('.answer');
-    for (let i = 0; i < arrayRadioAnswers.length; i++) {
-        if (arrayRadioAnswers[i].checked) {    
-            btnSend.disabled = false;
+    function enable() {
+        btnSend.disabled = true;
+        const arrayRadioAnswers = document.querySelectorAll('.answer');
+        for (let i = 0; i < arrayRadioAnswers.length; i++) {
+            if (arrayRadioAnswers[i].checked) {
+                btnSend.disabled = false;
+            }
         }
     }
-}
 
 //SELECCIONAR RESPUESTA Y PUNTOS
 
-btnSend.addEventListener('click', readUserAnswer);
-btnSend.addEventListener('click',paintQuestions);
+    btnSend.addEventListener('click', readUserAnswer);
+    btnSend.addEventListener('click', paintQuestions);
 
-function readUserAnswer() {
-    const arrayRadioAnswers = document.querySelectorAll('.answer');
+    function readUserAnswer() {
+        const arrayRadioAnswers = document.querySelectorAll('.answer');
 
-    for (let i = 0; i < arrayRadioAnswers.length; i++) {
-        if (arrayRadioAnswers[i].checked) {
-            optionChecked = arrayRadioAnswers[i];
+        for (let i = 0; i < arrayRadioAnswers.length; i++) {
+            if (arrayRadioAnswers[i].checked) {
+                optionChecked = arrayRadioAnswers[i];
+            }
         }
+        found = questionsWithAnswers.find(function (question) {
+            const questionBox = document.querySelector('.questionBox');
+            if (question.id === parseInt(questionBox.id)) {
+                return question
+            }
+        });
+        correctIncorrectAnswer(found, optionChecked)
     }
-    found = questionsWithAnswers.find(function(question) {
-        const questionBox = document.querySelector('.questionBox');
-        if (question.id === parseInt(questionBox.id)){
-            return question
-        }
-    });
-    correctIncorrectAnswer(found, optionChecked)
-}
 
-function correctIncorrectAnswer(){
-    if (found.answers[optionChecked.id].isCorrect === true){
-        console.log('BIEN');
-        msg.innerHTML = `<h3> ¡Correcta! </h3>`;
-        if (seconds <= 2) {
-            totalPoints += 2;
+    function correctIncorrectAnswer() {
+        if (found.answers[optionChecked.id].isCorrect === true) {
+            console.log('BIEN');
+            msg.innerHTML = `<h3> ¡Correcta! </h3>`;
+            if (seconds <= 2) {
+                totalPoints += 2;
+            }
+            else if (seconds >= 3 && seconds <= 10) {
+                totalPoints += 1;
+            }
+            else {
+                return totalPoints;
+            }
         }
-        else if  (seconds >= 3 && seconds <= 10){
-            totalPoints += 1;
+        else if (found.answers[optionChecked.id].isCorrect !== true) {
+            console.log('MAL');
+            msg.innerHTML = `<h3> ¡Incorrecta! </h3>`;
+            if (seconds >= 11) {
+                totalPoints -= 2;
+            }
+            else if (seconds <= 10) {
+                totalPoints -= 1;
+            }
         }
-        else {
-            return totalPoints;
-        }
+        updateScoreInterface();
+        console.log(totalPoints);
+        seconds = 0;
     }
-    else if (found.answers[optionChecked.id].isCorrect !== true) {
-        console.log('MAL');
-        msg.innerHTML = `<h3> ¡Incorrecta! </h3>`;
-        if (seconds >= 11) {
-            totalPoints -= 2;
-        }
-        else if  (seconds <= 10){
-            totalPoints -= 1;
-        }
+
+
+    function updateScoreInterface() {
+        scoreUI.innerHTML = ` ${totalPoints} puntos`
     }
-    printScoreUIRealTime();
-    console.log(totalPoints);
-    seconds = 0;
-}
-
-
-function printScoreUIRealTime() {
-    scoreUI.innerHTML = ` ${totalPoints} puntos`
-}
 
 // MARCADOR 
 
-btnSave.addEventListener('click', onSave);
+    btnSave.addEventListener('click', onSave);
 
-function onSave () {
-    saveScoreAndName();
-    resetTimeAndPoints();
-    cleanButtonsAndBoxes();
-}
+    function onSave() {
+        saveScoreAndName();
+        resetTimeAndPoints();
+        cleanButtonsAndBoxes();
+    }
 
-let score = { //Se guardan los nombres y las puntuaciones de cada jugador
+    let score = { //Se guardan los nombres y las puntuaciones de cada jugador
         names:
-        [],
+            [],
         points:
-        []
-};
+            []
+    };
 
-function saveScoreAndName() {
-    let name = document.querySelector('#inputNameId').value;
-    score.names.push(name);
-    listNames = score.names;
+    function saveScoreAndName() {
+        let name = document.querySelector('#inputNameId').value;
+        score.names.push(name);
+        listNames = score.names;
         console.log(listNames);
-    score.points.push(totalPoints);
-    sumPoints = score.points;
+        score.points.push(totalPoints);
+        sumPoints = score.points;
         console.log(sumPoints);
-    printScoreAndName(listNames, sumPoints)
-}
+        updateGeneralScoreboardInterface(listNames, sumPoints)
+    }
 
-function printScoreAndName() {
-    let scoreList = document.querySelector('.list');
-    let add = '';
-    for (let i = 0;i < listNames.length; i++){
-        add +=
-        `<li class="eachBoxPlayer">
+    function cleanButtonsAndBoxes() {
+        // let name = document.querySelector('#inputNameId').value = '';
+        btnStart.classList.toggle('invisible');
+        btnSend.classList.toggle('invisible');
+        boxQuestions.classList.add('invisible');
+        nameBox.classList.add('invisible');
+        msg.innerHTML = '';
+    }
+
+
+    function updateGeneralScoreboardInterface() {
+        let scoreList = document.querySelector('.list');
+        let add = '';
+        for (let i = 0; i < listNames.length; i++) {
+            add +=
+                `<li class="eachBoxPlayer">
             ${listNames[i]} - <div class="actualPoints"> ${sumPoints[i]} puntos </div>
         </li>`;
+        }
+        scoreList.innerHTML = add;
     }
-    scoreList.innerHTML= add;
-}
-
-
-
-
-
-function cleanButtonsAndBoxes(){
-    // let name = document.querySelector('#inputNameId').value = '';
-    btnStart.classList.toggle('invisible');
-    btnSend.classList.toggle('invisible');
-    boxQuestions.classList.add('invisible');
-    nameBox.classList.add('invisible');
-    msg.innerHTML='';
-}
 
 
 }
